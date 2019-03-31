ede653cce731f301fd49ae928d3225ea99bad580
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-03-31 08:48:36 -0700

    Added mobile menu

--- src/components/BackDrop/BackDrop.js
+++ src/components/BackDrop/BackDrop.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './BackDrop.css'
 
 import { NavigationLinks } from '../../navigation_links'
+// import BackDropImage from './assets/bigfoot.jpg'
 
 const BackDrop = props => {
     let backDropClass = props.backDropShown ? 'show' : 'hidden'
@@ -9,6 +10,7 @@ const BackDrop = props => {
     return (
         <div className={`Backdrop_container BackDrop ${backDropClass}`} onClick={props.backdropToggleHandler}>
             <div className="Backdrop_linkContainer">
+                <div className="BackDrop_backgroundImage" ></div>
                 <div className="Backdrop_closeButton">X</div>
                 <a href={`/`} >HOME</a>
 
