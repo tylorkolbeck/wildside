a12d2b3bede69190031565df2e0342dddf8b0e5c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-03-31 10:10:34 -0700

    added fonts

--- src/components/HomeDemo/DemoPlayer/DemoPlayer.js
+++ src/components/HomeDemo/DemoPlayer/DemoPlayer.js
@@ -3,13 +3,12 @@ import './DemoPlayer.css'
 
 const DemoPlayer = props => {
     return (
-        <div className="DemoPlayer_player_container">
-            <div>
+        <div>
                 <div key={props.playerObj.title} className="DemoPlayer_player">
                         <div className="DemoPlayer_play_button"></div>
-                    </div>
-                    <div className="DemoPlayer_description">{`${props.playerObj.title.toUpperCase()} ${props.playerObj.playTime}`}</div>
                 </div>
+                    
+                <div className="DemoPlayer_description">{`${props.playerObj.title.toUpperCase()} ${props.playerObj.playTime}`}</div>
         </div>
     )
 }
