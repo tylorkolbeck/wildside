e77ea239b13d52e86ae4064fb6c7bcdc25d4d387
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-04-04 22:01:41 -0700

    added demo mp3s and interface for playing demos

--- src/components/HomeDemo/DemoPlayer/DemoPlayer.js
+++ src/components/HomeDemo/DemoPlayer/DemoPlayer.js
@@ -3,11 +3,18 @@ import './DemoPlayer.css'
 
 
 const DemoPlayer = props => {
+    let playing = ''
+
+    if (props.playing) {
+        playing = 'DemoPlayer_notPlaying'
+        // playing = <div style={{background: 'red', zIndex: '100'}}>PLAYING</div>
+    }
     return (
         <div>
-                <div key={props.playerObj.title}  className="DemoPlayer_player" >
-                        <div className="DemoPlayer_play_button"></div>
+                <div key={props.playerObj.title}  className="DemoPlayer_player" data-title={props.dataTitle} id={props.id} onClick={props.playHandle}>
+                        <div className={`DemoPlayer_play_button ${playing}`}></div>
                 </div>
+                
                     
                 <div className="DemoPlayer_description">{`${props.playerObj.title.toUpperCase()} ${props.playerObj.playTime}`}</div>
         </div>
--- src/containers/Demos/Demos.js
+++ src/containers/Demos/Demos.js
@@ -1,62 +1,129 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Demos.css'
 
 import HeaderBox from '../../components/HeaderBox/HeaderBox'
-import DownArrow from '../../components/DownArrow/DownArrow'
+// import DownArrow from '../../components/DownArrow/DownArrow'
 import Shark1 from './assets/shark1_edited.jpg'
 import Player from '../../components/HomeDemo/DemoPlayer/DemoPlayer'
 
+import audio1 from './assets/audio/audio1.mp3'
+import audio2 from './assets/audio/audio2.mp3'
 
-let playerObjs = [
-    {
-        title: 'Aquarium Tour',
-        playTime: '1m 42s'
-    },
-    {
-        title: 'Dive School',
-        playTime: '42m'
-
-    },
-    // {
-    //     title: 'Big Cats',
-    //     playTime: '5m 34s'
-
-    // },
-    // {
-    //     title: 'Aquarium Tour 2',
-    //     playTime: '1m 42s'
-    // },
-    // {
-    //     title: 'Dive School 2',
-    //     playTime: '42m'
-
-    // },
-    // {
-    //     title: 'Big Cats 2',
-    //     playTime: '5m 34s'
-
-    // },
-]
-
-let players = playerObjs.map(player => {
-    return (
-        <Player playerObj={player} key={player.title}/>
-    )
-})
+import Sound from 'react-sound'
 
 
-const Demos = props => {
-    return (
-        <div>
-            <HeaderBox title="Demos" background={Shark1}>
-                <DownArrow style={{borderTop: '15px solid #606C67'}}/>
-            </HeaderBox>
+// let playerObjs = [
+//     {
+//         title: 'Aquarium Tour',
+//         playTime: '1m 42s',
+//         src: audio1,
+//         name: 'audio1',
+//     },
+//     {
+//         title: 'Dive School',
+//         playTime: '42m',
+//         src: audio2,
+//         name: 'audio2'
+//     },
+// ]
 
-            <div className="Demo_playerContainer">
-                {players}
+
+
+
+class Demos extends Component {
+
+    componentWillMount() {
+
+    }
+
+
+
+    state={
+
+        playerObjs: [
+            {
+                title: 'Aquarium Tour',
+                playTime: '1m 42s',
+                src: audio1,
+                name: 'audio1',
+                playing: false
+            },
+            {
+                title: 'Dive School',
+                playTime: '42m',
+                src: audio2,
+                name: 'audio2',
+                playing: false
+            },
+        ],
+
+        playing: false,
+
+        audio1 : {
+            src: audio1,
+        },
+
+        audio2 : {
+            src: audio2,
+        }
+    }
+
+    playAudioHandler(name, src) {
+        let playerObjsCopy = this.state.playerObjs
+
+        let playingDemo = playerObjsCopy.find(demo => demo.src === src)
+
+        if (playingDemo.playing === true) {
+            playingDemo.playing = false
+        } else {
+            playingDemo.playing = true
+        }
+
+        let notPlayingDemo = playerObjsCopy.find(demo => demo.src !== src)
+        notPlayingDemo.playing = false
+
+        if (this.state.playing === src) {
+            this.setState({playing: false}, () => {
+            })
+        } else {
+            this.setState({playing: this.state[name].src})
+        }
+    }
+
+    render() {
+        let players = this.state.playerObjs.map(player => {
+            return (
+                <Player 
+                    playerObj={player} 
+                    key={player.title} 
+                    id={player.src} 
+                    dataTitle={player.name} 
+                    playHandle={() => this.playAudioHandler(player.name, player.src)}
+                    playing={player.playing}/>
+            )
+        })
+
+        return (
+            <div className="Demo__container">
+                <Sound 
+                    url={this.state.playing ? this.state.playing : ''}
+                    playStatus={Sound.status.PLAYING}
+                    // onLoading={this.handleSongLoading}
+                    // onPlaying={this.handleSongPlaying}
+                    onFinishedPlaying={this.handleSongFinishedPlaying} />
+
+                <HeaderBox title="Demos" background={Shark1} className="testtest">
+                    {/* <DownArrow style={{borderTop: '15px solid #606C67'}}/> */}
+                </HeaderBox>
+    
+                <div className="Demo_playerContainer">
+                    {players}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
+
 
 export default Demos
